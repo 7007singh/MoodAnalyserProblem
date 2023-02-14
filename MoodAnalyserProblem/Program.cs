03456ecced6b549b84c3202ca188471c714d72cf
7007singh <shw12021982@gmail.com>
2023-02-14 11:19:23 +0530

    Welcome to Mood Analyzer program

---
+++ MoodAnalyserProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("___Welcome analyser problem___");
+        }
+    }
+}
